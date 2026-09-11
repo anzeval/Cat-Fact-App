f80b2f0fd709df821ce4dbe659150eaacd32201f
anzeval <anastasiiazelenska.work@gmail.com>
2026-09-11 10:11:04 +0200

    Add Cat Fact API request

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Net.Http;
+
+var client = new HttpClient();
+
+HttpResponseMessage response = await client.GetAsync("https://catfact.ninja/fact");
+
+if (response.IsSuccessStatusCode)
+{
+    string content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(content);
+}
+else
+{
+    Console.WriteLine($"Error: {response.StatusCode}");
+}
